b71e5b125a15ffbf1a1025525cad255d4958c3bc
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2025-05-07 18:33:37 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+// let a = 21;
+// let b = 222;
+// let c = 90;
+// let d = 898;
+// let e = 222;
+// let f = 876;
+// let g = 565;
+// let h = 101;
+// let i = a + b + c + d + e + f + g + h;
+// module.exports = i; 
+
+
+// module.exports.a = "sujal";
+// module.exports.b = "saraswat";
+// module.exports.c = "genius";
+// module.exports.d = "ronaldo";
+// module.exports.e = "messi";
+// module.exports.f = "Alan turing";
+
+
+// exports.a = 21;
+// exports.b = 23;
+// exports.c = 909;
+// exports.d = 333;
+// exports.e = 555;
+// exports.f = 666;
+// exports.g = 888;
+// exports.h = 101;
+
+
+//NO EXPORT NEEDED
+// var oneLinerJoke = require('one-liner-joke');
+// var getRandomJoke = oneLinerJoke.getRandomJoke();
+// console.log(getRandomJoke);
+
+
+
+
+//  NO EXPORT NEEDED
+// var figlet = require("figlet");
+
+// figlet("Hello World!!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
